6536316fc081ae19bb772f0a85ca2d6e5cfaecc2
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-09-05 23:36:46 +0200

    Added basic variables and global css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,12 @@
 <script>
-	/** @type {import('./$types').PageData} */
     import { SliceZone } from '@prismicio/svelte'
     import { components } from '$lib/slices'
 
 	export let data;
 </script>
 
-<!-- <h1>{data.page.data.example_title}</h1> -->
-
 <SliceZone slices={data.slices} {components}/>
 
-<pre>
-    <!-- {JSON.stringify(data.slices, null, 2)} -->
-</pre>
+<!-- <pre>
+    {JSON.stringify(data.slices, null, 2)}
+</pre> -->
