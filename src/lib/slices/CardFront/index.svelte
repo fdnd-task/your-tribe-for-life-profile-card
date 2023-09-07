ffd78724b0245bf5965f1e6d45deb4b93dc12aed
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-09-07 23:35:46 +0200

    Initial try card tilt

--- src/lib/slices/CardFront/index.svelte
+++ src/lib/slices/CardFront/index.svelte
@@ -1,9 +1,61 @@
 <script>
 	/** @type {import("@prismicio/client").Content.CardFrontSlice} */
+    let cardFront
+    let glare
+    let animated = false
+
+    function isAnimated(){
+        animated = !animated
+    }
+
+    function calculateAngle(e){
+
+        // console.log(e)
+
+        let shadow = 'rgba(255, 255, 255, 0.3)'
+
+        isAnimated()
+
+        let x = Math.abs(cardFront.getBoundingClientRect().x - e.clientX)
+        let y = Math.abs(cardFront.getBoundingClientRect().y - e.clientY)
+
+        let halfWidth = cardFront.getBoundingClientRect().width / 2
+        let halfHeight = cardFront.getBoundingClientRect().height / 2
+
+        // console.log('Half Width:', halfWidth)
+        // console.log('Half Height:', halfHeight)
+        // console.log('X Position:', x)
+        // console.log('Y Position:', y)
+
+        let angleX = (x - halfWidth) / 6
+        let angleY = (y - halfHeight) / 14
+
+        console.log('Angle X:', angleX)
+        console.log('Angle Y:', angleY)
+
+        let glareX = (1 - (x / (halfWidth * 2))) * 100
+        let glareY = (1 - (y / (halfHeight * 2))) * 100
+
+        glare.style.background = `radial-gradient(circle at ${glareX}% ${glareY}%, rgb(199 198 243), transparent)`
+        cardFront.style.perspective = `${halfWidth * 6}px`
+        cardFront.style.transform = `rotateY(${angleX}deg) rotateX(${-angleY}deg) scale(1.04)`
+
+        let shadowX = (x - halfWidth) / 3
+        let shadowY = (y - halfHeight) / 6
+
+        cardFront.style.filter = `drop-shadow(${-shadowX}px ${-shadowY}px 15px ${shadow})`
+    }
+
+    function resetCard(){
+        cardFront.style.transform = `rotateY(0deg) rotateX(0deg) scale(1)`
+        cardFront.style.filter = `drop-shadow(0px 0px 0px transparent)`
+    }
+
 	export let slice;
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+<section bind:this={cardFront} on:mouseenter={calculateAngle} on:mousemove={calculateAngle} on:mouseleave={resetCard} class:animated={isAnimated} data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+    <span bind:this={glare}></span>
 	<h1>{slice.primary.heading}</h1>
 </section>
 
@@ -47,5 +99,22 @@
     h1{
         color: white;
         opacity: 1;
+    }
+
+    section span{
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        transition: all 0.1s ease-out;
+        opacity: 0.6;
+        pointer-events: none;
+        height: 100%;
+        border-radius: 14px;
+        z-index: 9999;
+        mix-blend-mode: hard-light;
+        background: radial-gradient(circle at 50% 50%, rgb(199 198 243), transparent);
+        opacity: 0;
     }
 </style>
